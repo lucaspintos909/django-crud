88c885330d3742914c3c55cae8e6ae4bff3b0278
Lucas Pintos <lucaspintos909@gmail.com>
2022-02-05 18:45:34 -0300

    feat: Agregando url para eliminar el libro

--- libreria/urls.py
+++ libreria/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('libros', views.books, name="books"),
     path('libros/ingresar', views.create_book, name="create_book"),
     path('libros/editar', views.edit_book, name="edit_book"),
+    path('libros/delete/<int:id>', views.delete_book, name="delete_book"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
