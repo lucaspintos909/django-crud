80d6770ed9f33d5dfeafa0a1462be17eccec029b
Lucas Pintos <lucaspintos909@gmail.com>
2022-02-04 22:16:21 -0300

    feat: Agregando vista "nosotros"

--- libreria/views.py
+++ libreria/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+  return HttpResponse("<h1>Bienvenido a la libreria</h1>")
+
+def nosotros(request):
+  return render(request, 'pages/nosotros.html')
