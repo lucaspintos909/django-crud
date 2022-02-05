f105f5fc5b577a0fa22b6862114e1143a163ab1a
Lucas Pintos <lucaspintos909@gmail.com>
2022-02-05 17:11:39 -0300

    feat: Configurando rutas para estaticos (imagenes)

--- libreria/urls.py
+++ libreria/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+
 from . import views
 
 urlpatterns = [
@@ -6,4 +9,4 @@ urlpatterns = [
     path('libros', views.books, name="books"),
     path('libros/ingresar', views.create_book, name="create_book"),
     path('libros/editar', views.edit_book, name="edit_book"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
