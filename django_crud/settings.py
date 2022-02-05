339ad37e4dc2d8f7ba9a8c7aa83d116f05e7204d
Lucas Pintos <lucaspintos909@gmail.com>
2022-02-05 17:09:42 -0300

    feat: Añadiendo media urls en settings
    Declarando ruta de la carpeta de images para usar las imagenes

--- django_crud/settings.py
+++ django_crud/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -119,7 +120,8 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+MEDIA_ROOT=os.path.join(BASE_DIR, "")
+MEDIA_URL="/images/"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
