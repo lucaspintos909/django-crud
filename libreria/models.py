c250f4d40e704b5b7fbd74b4c486e4979a69871f
Lucas Pintos <lucaspintos909@gmail.com>
2022-02-05 13:53:52 -0300

    feat: Modelo Libro

--- libreria/models.py
+++ libreria/models.py
@@ -1,3 +1,17 @@
+from typing import Any
 from django.db import models
 
 # Create your models here.
+class Book(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=50, verbose_name="Título")
+    description = models.TextField(null=True, verbose_name="Descripción")
+    image = models.ImageField(upload_to="images/", verbose_name="Imagen", null=True)
+
+    def delete(self, using = None, keep_parents = False):
+        self.image.storage.delete(self.image.name)
+        super().delete()
+
+    def __str__(self):
+        row = f'Libro: {self.title}'
+        return row
