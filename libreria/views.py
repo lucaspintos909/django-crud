b7ebdfdf3ddc6a465c62c21e0d465b3561fe2a2e
Lucas Pintos <lucaspintos909@gmail.com>
2022-02-05 17:10:46 -0300

    feat: Pasandole todos los libros a la vista

--- libreria/views.py
+++ libreria/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
+from .models import Book
 
 # Create your views here.
 def index(request):
     return render(request, 'pages/index.html')
 
 def books(request):
-    return render(request, 'books/index.html')
+    books = Book.objects.all();
+    return render(request, 'books/index.html', {"books": books})
 
 def create_book(request):
     return render(request, 'books/create.html')
