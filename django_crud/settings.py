8937d544dc86398229b004765b0c6ace9f52e69c
Lucas Pintos <lucaspintos909@gmail.com>
2022-02-05 11:24:16 -0300

    feat: Datos para conexion a la base de datos

--- django_crud/settings.py
+++ django_crud/settings.py
@@ -73,11 +73,18 @@ WSGI_APPLICATION = 'django_crud.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+""" 'default': {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': BASE_DIR / 'db.sqlite3',
+} """
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': '0.0.0.0',
+        'NAME': 'libreria',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'PORT': '3306'
     }
 }
 
