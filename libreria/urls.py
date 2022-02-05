48d7a78feb378dd93ceeb4427344cf5dd94cc906
Lucas Pintos <lucaspintos909@gmail.com>
2022-02-05 18:59:37 -0300

    feat: Agregando parametro id en al url

--- libreria/urls.py
+++ libreria/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('libros', views.books, name="books"),
     path('libros/ingresar', views.create_book, name="create_book"),
-    path('libros/editar', views.edit_book, name="edit_book"),
+    path('libros/editar/<int:id>', views.edit_book, name="edit_book"),
     path('libros/delete/<int:id>', views.delete_book, name="delete_book"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
