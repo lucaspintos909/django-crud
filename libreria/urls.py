b0c57d4371de14fe14883e37bae23bbe96a4ad53
Lucas Pintos <lucaspintos909@gmail.com>
2022-02-05 10:45:59 -0300

    feat: Url para editar libros

--- libreria/urls.py
+++ libreria/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('libros', views.books, name="books"),
     path('libros/ingresar', views.create_book, name="create_book"),
+    path('libros/editar', views.edit_book, name="edit_book"),
 ]
